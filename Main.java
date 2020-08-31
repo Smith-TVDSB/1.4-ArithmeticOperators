1e7c59d72d5e5bc83be874cd48777b6e8426be79
Keith Smith <keith.smith@tvdsb.ca>
2020-08-31 13:10:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+   
+   System.out.println(5+4); // this will output 9
+   System.out.println(5-3); // this will output 2
+   System.out.println(2*5); // this will output 10
+   System.out.println(8/4); // this will output 2
+   System.out.println(10%8); // this will output 2
+
+   System.out.print("\n");
+
+   //You can also use variables
+    int x=8, y=5;
+    System.out.println(x+y); // this will output 13
+    System.out.println(x-y); // this will output 3
+    System.out.println(x*y); // this will output 40
+    System.out.println(x/y); // this will output 1 because there's no decimals in integers
+    System.out.println(x%y); // this will output 3
+    System.out.print("\n");
+
+    int sum;
+    sum = x+y; // 8+5 = 13 so z = 13
+    System.out.println(sum);
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+#1.4a Exemplary Program
+
+Modify the program to output something different using the same arithmetic operators. 
+
+Comment at teh end of the program which one was easier to modify and why.
